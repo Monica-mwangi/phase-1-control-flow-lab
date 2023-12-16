1753fceab75316d36906922b9c4d927e80971315
Monica <monica@DESKTOP-A3SK9UM.localdomain>
2023-12-16 13:06:56 -0800

    done with assignement

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400){
+    return 'This one is on me!';
+  }
+  else if (someValue > 2500){
+    return 'No can do.';
+  }
+  else if (someValue > 2000){
+    return 'I will gladly take your thirty bucks.';
+  }
 }
+console.log(scuberGreetingForFeet(2501));
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'? "Ok, sounds good." :"No go.";
+
+
 }
+ternaryCheckCity('Pittsburgh')
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(response){
   // Write your code here!
+  switch(response){
+    case response = 'generous':
+      return 'Thank you so much.'
+      case response = 'not as generous':
+      return 'Thank you.'
+      default:
+        return 'Bye.'
+
+  }
 }
